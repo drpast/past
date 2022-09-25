3f925a0040e51e7ebb76289bceb12de231c668ae
drpast <114338996+drpast@users.noreply.github.com>
2022-09-25 17:41:47 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -3,7 +3,7 @@ var genderx=document.getElementById("gender").value;
 var birthx=document.getElementById("birth").value;
 if(genderx=="" || birthx==""){
   document.getElementById("gender").style.color="#FFC300";
-  document.getElementById("birth").style.color="#FFC300";
+  document.getElementById("birth").style.color="red";
 }
 else{
   document.getElementById("gender").style.color="black";
